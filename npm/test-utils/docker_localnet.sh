4a455408371dbf67fa7f9421a5da1c3b710efc6a
ben <benjamin.h.67110@gmail.com>
2025-11-22 08:35:06 +0100

    v1.1.0

--- npm/test-utils/docker_localnet.sh
+++ npm/test-utils/docker_localnet.sh
@@ -27,7 +27,7 @@ VALIDATOR_KEY_NAME="${VALIDATOR_KEY_NAME:-validator}"
 mkdir -p "$NODE_HOME"
 mkdir -p "$BIN_CACHE"
 
-DEFAULT_RELEASE_URL="https://github.com/network-lumen/blockchain/releases/download/v1.0.1/linux-amd64-v1.0.1.tar.gz"
+DEFAULT_RELEASE_URL="https://github.com/network-lumen/blockchain/releases/download/v1.1.0/linux-amd64-v1.1.0.tar.gz"
 RELEASE_URL="${LUMEN_RELEASE_URL:-$DEFAULT_RELEASE_URL}"
 ARCHIVE_NAME="$(basename "$RELEASE_URL")"
 
